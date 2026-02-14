115cd37bc00f8e0a06360577e64a125db563fda0
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-14 22:18:23 +0530

    Practical1 done

---
+++ Practical1.py
@@ -0,0 +1,18 @@
+# This file shows the basics of python 
+# sum
+a = 1
+b = 2
+c = a + b
+print ("Sum is: ", c)
+
+# types of data type using type keyword
+a = 1
+b = "Rabbit"
+c = 2.5
+d = True
+e = None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
