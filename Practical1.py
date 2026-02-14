987724608765f11e5182ce98c748fc390c9c80d0
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-14 23:45:31 +0530

    Basics1 done

--- Practical1.py
+++ Practical1.py
@@ -1,12 +1,12 @@
 # This file shows the basics of python 
 # sum
-'''
 a = 1
 b = 2
 c = a + b
 print ("Sum is: ", c)
 
 # types of data type using type keyword
+'''
 a = 1
 b = "Rabbit"
 c = 2.5
@@ -30,9 +30,26 @@ b = int("5")
 c = a + b
 print(c)
 
-'''
 # taking input from user and printing them
 a = int(input("enter value 1: "))
 b = int(input("enter value 2: "))
 c = a + b
 print("Sum is: ", c)
+
+# code for area of square
+s = float(input('Enter the side of square: '))
+Area = s * s
+print('Area of give square is: ', Area)
+
+# average
+a = float(input("enter value 1: "))
+b = float(input("enter value 2: "))
+c = a + b
+d = c/2
+print("Average is: ", d)
+'''
+# Boolean
+a = int(input("enter value 1: "))
+b = int(input("enter value 2: "))
+c = (a >= b)
+print(c)
