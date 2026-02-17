caba498feea05fdd44bad4a7553b532a61825bb2
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-17 12:33:53 +0530

    Calculated average marks of students using Class and Object

--- src/OOPs.py
+++ src/OOPs.py
@@ -1,5 +1,14 @@
 class Student:
-    name = "Kashish"
-s1 = Student()
-print(s1.name)
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def average(self):
+        total = sum(self.marks)
+        print("Hello,", self.name, "Your average score is: ", total/5 )
+
+        
+
+s1 = Student("Kashish", [98, 95, 99, 92, 91])
+s1.average()  
 
