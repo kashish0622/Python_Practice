fde5557ba0babd068bdaae2da49ad00d0d9e1403
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-18 12:44:54 +0530

    added a practical of multi_level inheritance

--- src/object_oriented_programming/Inheritance.py
+++ src/object_oriented_programming/Inheritance.py
@@ -1,5 +1,5 @@
-# Inheritance 
-class Person:
+# Inheritance 1 single-level
+'''class Person:
     def __init__(self, name, age):
         self.name= name
         self.age= age
@@ -19,4 +19,48 @@ s1 = Student("Anuj Kumar", 16, "STUD101")
 
 s1.display_person()
 s1.display_student()
+'''
+# Inheritance 2 multi-level
+#LibrarymanagementsystemusingOOPs
+class LibraryItem():
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    def display_info(self):
+        print("Title of book is: ", self.title)
+        print("Author: ", self.author)
 
+class Books(LibraryItem):
+    def __init__(self, title, author, pages):
+        super().__init__(title, author) 
+        self.pages = pages
+    def display_info(self):
+        super().display_info()
+        print("The no. of pages are: ", self.pages)
+        print("Type: BOOK")
+
+class Magazine(LibraryItem):
+    def __init__(self, title, author, issue_number):
+        super().__init__(title, author)
+        self.issue_number = issue_number
+    def display_info(self):
+        super().display_info()
+        print("The issue number is: ", self.issue_number)
+        print("Type: MAGAZINE")
+
+class DVD(LibraryItem):
+    def __init__(self, title, author, duration):
+        super().__init__(title, author)
+        self.duration = duration
+    def display_info(self):
+        super().display_info()
+        print("Duration of the DVD is: ", self.duration, "minutes")
+        print("Type: DVD")
+
+book1 = Books("It starts with us", "Colleen Hoover", 400)
+magazine1 = Magazine("Vogue", "Editor team", 101 )
+dvd1 = DVD("Iot Basics", "Quadratech_team", 55 ) 
+       
+book1.display_info()
+magazine1.display_info()
+dvd1.display_info()
