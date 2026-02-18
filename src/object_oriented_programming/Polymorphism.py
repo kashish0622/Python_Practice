a271322536b8c9951c6c592d1d78b047cf591299
kashish0622 <kashishbhattofficial@gmail.com>
2026-02-18 14:29:59 +0530

    added practical of method overload

--- src/object_oriented_programming/Polymorphism.py
+++ src/object_oriented_programming/Polymorphism.py
@@ -1,5 +1,6 @@
 # Method override
 #EmployeeSalarySystem
+'''
 class Employee:
     def calculate_salary(self):
         print("Salary calculation depends on the employee type")
@@ -27,3 +28,21 @@ Employees = [
 
 for emp in Employees:
     emp.calculate_salary()
+    '''
+# method overload
+class Bonus:
+    def calculate_bonus(self, salary, rating = None):
+        self.salary=salary
+        self.rating=rating
+
+        if rating is None:
+            bonus=salary * 0.20
+            print("Bonus with salary only: ", bonus)
+
+        else:
+            bonus=salary*(0.20 + rating * 0.10)
+            print("Bonus with salary and rating: ", bonus)
+
+a = Bonus()
+a.calculate_bonus(500)
+a.calculate_bonus(500, 5)
